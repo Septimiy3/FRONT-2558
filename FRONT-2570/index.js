c59afb72d6f4613089b527f5ef5e37c6868423e5
Dmitry Kalabin <septimiy64@gmal.com>
2024-11-14 22:25:18 +0800

    добавлены форматирования дат

---
+++ FRONT-2570/index.js
@@ -0,0 +1,25 @@
+const dateText = document.querySelector('.date-format-option')
+const dateTextNonFormat = document.querySelector('.data-format-non-format')
+const dateTextIso = document.querySelector('.data-format-iso')
+const dateTextIsoFormate = document.querySelector('.data-format-iso-format')
+
+const options = {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    hour12:false,
+    hour: 'numeric',
+    minute: 'numeric',
+}
+const newDate = new Date()
+console.log(newDate)
+
+const dateStr = "2024-11-14T13:49:22.1695002"
+const date = new Date(dateStr)
+
+
+
+dateText.textContent = newDate.toLocaleDateString('ru',options)
+dateTextNonFormat.textContent = dateStr
+dateTextIso.textContent = date
+dateTextIsoFormate.textContent = date.toLocaleString('ru')
