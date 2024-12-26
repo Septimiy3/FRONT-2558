b4e66fb383c33085a9af893e426f32b704c8bc6e
Dmitry Kalabin <septimiy64@gmal.com>
2024-12-26 14:32:10 +0800

    Front-2571 рефакторинг

--- FRONT-2571/index.js
+++ FRONT-2571/index.js
@@ -1,16 +1,27 @@
-const one = document.querySelector('#one')
-const two = document.querySelector('#two')
-const three = document.querySelector('#three')
-const four = document.querySelector('#four')
-const five = document.querySelector('#five')
-const six = document.querySelector('#six')
-const seven = document.querySelector('#seven')
+const enEuFormat = document.querySelector(".en-eu-format");
+const ruFormat = document.querySelector(".ru-format");
+const deFormat = document.querySelector(".de-format");
+const decimalNumber = document.querySelector(".decimal-number");
+const ruCurrency = document.querySelector(".ru-currency");
+const ruCurrencyIntl = document.querySelector(".ru-currency-intl");
+const japCurrencyIntl = document.querySelector(".jap-currency-intl");
 
-const number = 1000000
+const number = 1000000;
 const numberDecimal = 1234.56789989;
 
-one.textContent = number.toLocaleString('en-US')
-two.textContent = number.toLocaleString('ru')
-three.textContent = number.toLocaleString('de-DE')
-four.textContent = numberDecimal.toFixed(3)
-five.textContent = number.toLocaleString('en-US', { style: 'currency', currency: 'rub' });
+enEuFormat.textContent = number.toLocaleString("en-US");
+ruFormat.textContent = number.toLocaleString("ru");
+deFormat.textContent = number.toLocaleString("de-DE");
+decimalNumber.textContent = numberDecimal.toFixed(3);
+ruCurrency.textContent = number.toLocaleString("en-US", {
+  style: "currency",
+  currency: "rub",
+});
+ruCurrencyIntl.textContent = new Intl.NumberFormat("ru", {
+  style: "currency",
+  currency: "rub",
+}).format(number);
+japCurrencyIntl.textContent = new Intl.NumberFormat("JPY", {
+  style: "currency",
+  currency: "JPY",
+}).format(number);
