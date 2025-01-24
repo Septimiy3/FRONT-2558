3cd7a119c9b2f618af683c11383330ec113aa69d
Dmitry <septimiy64@gmail.com>
2025-01-24 14:04:42 +0800

    рефакторинг

--- FRONT-2570/index.js
+++ FRONT-2570/index.js
@@ -1,8 +1,9 @@
-const dateText = document.querySelector('.date-format-option')
+const dateTextFormatOption = document.querySelector('.date-format-option')
 const dateTextNonFormat = document.querySelector('.data-format-non-format')
-const dateTextIso = document.querySelector('.data-format-iso')
-const dateTextIsoFormat = document.querySelector('.data-format-iso-format')
+const dateText = document.querySelector('.date-format')
+const dateTextLocaleRu = document.querySelector('.data-format-locale-ru')
 const dateTextIntlFormat = document.querySelector('.data-format-intl-format')
+const dateFormat = document.querySelector(".data-format-get-date");
 
 const options = {
     day: 'numeric',
@@ -17,14 +18,16 @@ const dateNow = Date.now()
 
 const dateStr = "2024-11-14T13:49:22.1695002"
 const date = new Date(dateStr)
+const currentDay = new Date()
 
 
 const formatDate = new Intl.DateTimeFormat('ru', options)
 
 
 
-dateText.textContent = newDate.toLocaleDateString('ru',options)
+dateTextFormatOption.textContent = newDate.toLocaleDateString('ru',options)
 dateTextNonFormat.textContent = dateStr
-dateTextIso.textContent = date
-dateTextIsoFormat.textContent = date.toLocaleString('ru')
+dateText.textContent = date
+dateTextLocaleRu.textContent = date.toLocaleString('ru')
 dateTextIntlFormat.textContent = formatDate.format(dateNow)
+dateFormat.textContent = `${currentDay.getDate() + '.'}${currentDay.getMonth() + 1 + '.'}${currentDay.getFullYear()}`
